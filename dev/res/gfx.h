1faedb09f4cbd1313fa0f528f36cbcd8e3149f2f
StevePro7 <steveprozune@hotmail.com>
2020-11-04 20:12:07 +0000

    no message

---
+++ dev/res/gfx.h
@@ -0,0 +1,26 @@
+#ifndef _RES_GFX_H_
+#define _RES_GFX_H_
+
+extern const SpriteDefinition gfx_actor;
+extern const SpriteDefinition gfx_right;
+extern const SpriteDefinition gfx_wrong;
+extern const Image gfx_eddie_01;
+extern const Image gfx_eddie_02;
+extern const Image gfx_eddie_03;
+extern const Image gfx_eddie_04;
+extern const Image gfx_eddie_05;
+extern const Image gfx_eddie_06;
+extern const Image gfx_eddie_07;
+extern const Image gfx_eddie_08;
+extern const Image gfx_eddie_09;
+extern const Image gfx_eddie_10;
+extern const Image gfx_eddie_12;
+extern const Image gfx_eddie_11;
+extern const Image gfx_eddie_13;
+extern const Image gfx_eddie_14;
+extern const Image gfx_eddie_16;
+extern const Image gfx_eddie_17;
+extern const Image gfx_eddie_18;
+extern const Image gfx_splash;
+
+#endif // _RES_GFX_H_
