b0bab6bebf435679011eda3f52c6b3c5b70d242a
StevePro7 <steveprozune@hotmail.com>
2020-11-05 20:52:56 +0000

    no message

--- dev/src/main.c
+++ dev/src/main.c
@@ -3,19 +3,26 @@
 int main()
 {
 	u16 *data = NULL;
+	unsigned char input;
 	Image image;
 
-	image = gfx_eddie_09;
+	image = gfx_eddie_07;
 	if( NULL != image.palette )
 	{
 		data = image.palette->data;
 	}
 
 	VDP_setPalette( PAL1, data );
-	VDP_drawImageEx( BG_A, &image, TILE_ATTR_FULL( PAL1, 0, 0, 0, 1 ), 0, 0, 0, CPU );
+	//VDP_drawImageEx( BG_A, &image, TILE_ATTR_FULL( PAL1, 0, 0, 0, 1 ), 0, 0, 0, CPU );
 	while( 1 )
 	{
-		//wait for screen refresh
+		engine_input_manager_update();
+		input = engine_input_manager_hold_right();
+		if( input )
+		{
+			VDP_drawImageEx( BG_A, &image, TILE_ATTR_FULL( PAL1, 0, 0, 0, 1 ), 0, 0, 0, CPU );
+		}
+
 		VDP_waitVSync();
 	}
 
