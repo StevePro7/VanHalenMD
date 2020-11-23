48d521f450ab713220160d3c053cff72b60dd6ab
StevePro7 <steveprozune@hotmail.com>
2020-11-23 21:02:07 +0000

    no message

--- dev/src/main.c
+++ dev/src/main.c
@@ -13,10 +13,10 @@ int main()
 	//TODO move into init_screen.
 
 	//open_screen_type = screen_type_func;
-	//open_screen_type = screen_type_begin;
+	open_screen_type = screen_type_begin;
 	//open_screen_type = screen_type_intro;
 	//open_screen_type = screen_type_init;
-	open_screen_type = screen_type_riff;
+	//open_screen_type = screen_type_riff;
 	//open_screen_type = screen_type_diff;
 	//open_screen_type = screen_type_ready;
 	//open_screen_type = screen_type_load;
