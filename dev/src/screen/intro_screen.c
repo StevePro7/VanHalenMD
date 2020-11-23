a15fc4c4f655ee15914e5394667397f72112f235
StevePro7 <steveprozune@hotmail.com>
2020-11-23 21:27:58 +0000

    no message

--- dev/src/screen/intro_screen.c
+++ dev/src/screen/intro_screen.c
@@ -6,7 +6,8 @@
 void screen_intro_screen_load()
 {
 	engine_graphics_manager_clear_bottom();
-	engine_font_manager_text( "INTRO SCREEN!!", 10, 2 );
+	//engine_font_manager_text( "INTRO SCREEN!!", 10, 2 );
+	engine_font_manager_text( "Continue on to the next screens...!", 5, 24 );
 }
 
 void screen_intro_screen_update( unsigned char *screen_type )
