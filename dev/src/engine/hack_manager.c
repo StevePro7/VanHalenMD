2d35de8751ee9377523e1a6a7ef38772fe299768
StevePro7 <steveprozune@hotmail.com>
2020-11-23 21:17:38 +0000

    no message

--- dev/src/engine/hack_manager.c
+++ dev/src/engine/hack_manager.c
@@ -6,8 +6,8 @@ struct_hack_object global_hack_object;
 void engine_hack_manager_init()
 {
 	struct_hack_object *ho = &global_hack_object;
-	ho->hack_delayspeed = 1;
-	//ho->hack_delayspeed = 0;
+	//ho->hack_delayspeed = 1;
+	ho->hack_delayspeed = 0;
 }
 
 void engine_hack_manager_invert()
