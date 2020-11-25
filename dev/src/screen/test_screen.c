f54bbf4db2960bd77cac4b1f27df20a9b55415d5
StevePro7 <steveprozune@hotmail.com>
2020-11-25 21:38:54 +0000

    no message

--- dev/src/screen/test_screen.c
+++ dev/src/screen/test_screen.c
@@ -11,8 +11,6 @@ void screen_test_screen_load()
 
 	////engine_font_manager_char( '*', 2, 6 ); engine_font_manager_char( '*', 2, 7 );
 	////engine_sprite_manager_draw( sprite_type_chose );
-
-	engine_image_manager_draw_banner();
 }
 
 void screen_test_screen_update( unsigned char *screen_type )
