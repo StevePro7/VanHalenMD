89c91017486d433f55c5da5a8e591829915e4db5
StevePro7 <steveprozune@hotmail.com>
2020-11-30 20:57:54 +0000

    no message

--- dev/src/main.c
+++ dev/src/main.c
@@ -15,10 +15,10 @@ int main()
 
 	//open_screen_type = screen_type_func;
 	//open_screen_type = screen_type_splash;
-	open_screen_type = screen_type_init;
-	//open_screen_type = screen_type_title;
+	//open_screen_type = screen_type_init;
+	open_screen_type = screen_type_title;
 	//open_screen_type = screen_type_begin;
-	//open_screen_type = screen_type_intro;
+	//open_screen_type = screen_type_start;
 	//open_screen_type = screen_type_riff;
 	//open_screen_type = screen_type_diff;
 	//open_screen_type = screen_type_ready;
--- dev/src/screen/start_screen.c
+++ dev/src/screen/start_screen.c
@@ -1,13 +1,99 @@
 #include "start_screen.h"
+#include "eddie_manager.h"
 #include "enum_manager.h"
 #include "font_manager.h"
+#include "game_manager.h"
+#include "graphics_manager.h"
+#include "hack_manager.h"
+#include "image_manager.h"
+#include "input_manager.h"
+#include "locale_manager.h"
+#include "quiz_manager.h"
+#include "random_manager.h"
+#include "sprite_manager.h"
+#include "text_manager.h"
+#include "timer_manager.h"
+
+#define START_FLASH_DELAY	50
+#define LOCAL_CHEAT_TOTAL	5
+
+static unsigned char event_stage;
+static unsigned char flash_count;
+static unsigned char cheat_count;
 
 void screen_start_screen_load()
 {
-	engine_font_manager_text( "START SCREEN!!", 2, 10 );
+	struct_hack_object *ho = &global_hack_object;
+	//engine_font_manager_text( "START SCREEN!!", 10, 2 );
+
+	//engine_font_manager_text( "TITLE SCREEN...!", 4, 10 );
+	//engine_font_manager_text( "123456789A123456789A", 0, 26 );
+
+	engine_font_manager_text( LOCALE_TITLE_VINTAGE, 7, 7 );
+	engine_font_manager_text( LOCALE_TITLE_VANHALEN, 6, 8 );
+
+	engine_text_manager_draw_lines( 30, 5 );
+
+	engine_game_manager_set_local_cheat( completion_type_nop );
+	if( ho->hack_invincible )
+	{
+		engine_font_manager_text( LOCALE_CHEAT_MARKER, 18, 27 );
+		engine_game_manager_set_local_cheat( completion_type_yes );
+	}
+
+	engine_font_manager_text( LOCALE_TITLE_START, 5, 24 );
+	engine_delay_manager_load( START_FLASH_DELAY );
+	event_stage = event_stage_start;
+	flash_count = 0;
+	cheat_count = 0;
 }
 
 void screen_start_screen_update( unsigned char *screen_type )
 {
+	struct_hack_object *ho = &global_hack_object;
+	unsigned char delay;
+	unsigned char input;
+	unsigned char input2;
+
+	engine_sprite_manager_update();
+	delay = engine_delay_manager_update();
+	if( delay )
+	{
+		if( !ho->hack_delayspeed )
+		{
+			flash_count = 1 - flash_count;
+		}
+
+		if( flash_count )
+		{
+			engine_font_manager_text( LOCALE_BLANK_SIZE18, 2, 24 );
+		}
+		else
+		{
+			engine_font_manager_text( LOCALE_TITLE_START, 5, 24 );
+		}
+	}
+
+	input = engine_input_manager_hold_buttonA();
+	input2 = engine_input_manager_hold_buttonStart();
+	if( input || input2 )
+	{
+		//TODO testing
+		//engine_quiz_manager_set_riffs_play( 0 );
+		//engine_quiz_manager_set_difficulty( difficulty_type_easy );
+		//TODO testing
+
+		//*screen_type = screen_type_load;
+		*screen_type = screen_type_riff;
+		//*screen_type = screen_type_ready;
+		//*screen_type = screen_type_func;
+		return;
+	}
+
+	engine_random_manager_rand();
+	engine_sprite_manager_update();
 	*screen_type = screen_type_start;
+	//*screen_type = screen_type_riff;
+	//*screen_type = screen_type_load;
+	//*screen_type = screen_type_ready;
 }
