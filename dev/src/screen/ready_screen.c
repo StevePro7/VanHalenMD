3c5af60b79cd1fa1d9066cb4c2689f238d1a73fb
StevePro7 <steveprozune@hotmail.com>
2020-12-02 16:40:10 +0000

    no message

--- dev/src/screen/ready_screen.c
+++ dev/src/screen/ready_screen.c
@@ -1,18 +1,23 @@
 #include "ready_screen.h"
-//#include "cursor_manager.h"
-#include "eddie_manager.h"
+#include "audio_manager.h"
+//#include "eddie_manager.h"
 #include "enum_manager.h"
 #include "font_manager.h"
-#include "hack_manager.h"
+//#include "hack_manager.h"
 #include "graphics_manager.h"
-#include "image_manager.h"
+//#include "image_manager.h"
 #include "input_manager.h"
 #include "locale_manager.h"
 #include "quiz_manager.h"
 #include "sprite_manager.h"
-#include "storage_manager.h"
+//#include "storage_manager.h"
 #include "text_manager.h"
-//#include "timer_manager.h"
+#include "timer_manager.h"
+
+#define READY_SCREEN_DELAY		100
+#define READY_SCREEN_PAUSE		150
+
+static unsigned char stage;
 
 void screen_ready_screen_load()
 {
@@ -33,21 +38,55 @@ void screen_ready_screen_load()
 	engine_text_manager_draw_lines_palette( riff_line2[ qo->quiz_riff_numbs ], 4, 12 );
 	engine_text_manager_draw_lines_palette( diff_line1[ qo->quiz_difficulty ], 4, 20 );
 	engine_text_manager_draw_lines_palette( diff_line2[ qo->quiz_difficulty ], 4, 21 );
+
+	engine_delay_manager_load( READY_SCREEN_DELAY );
+	stage = event_stage_start;
 }
 
 void screen_ready_screen_update( unsigned char *screen_type )
 {
+	unsigned char delay;
 	unsigned char input;
 
 	engine_sprite_manager_update();
 	input = engine_input_manager_hold_buttonA();
 	if( input )
 	{
-		//engine_font_manager_text( "begin", 0, 0 );
-		//engine_quiz_manager_set_quiz_saved_all();
-
+		engine_audio_manager_stop();
 		*screen_type = screen_type_load;
 		return;
+	}
+
+	/*if( event_stage_menus == stage )
+	{
+		delay = engine_delay_manager_update();
+		if( delay )
+		{
+			*screen_type = screen_type_load;
+		}
+
+		return;
+	}*/
+
+	if( event_stage_pause == stage )
+	{
+		delay = engine_delay_manager_update();
+		if( delay )
+		{
+			engine_audio_manager_stop();
+			*screen_type = screen_type_load;
+			return;
+		}
+
+		return;
+	}
+
+	delay = engine_delay_manager_update();
+	if( delay )
+	{
+		engine_audio_manager_play_effect( effects_type_ready );
+		engine_delay_manager_load( READY_SCREEN_PAUSE );
+		stage = event_stage_pause;
 	}
 
 	engine_sprite_manager_update();
