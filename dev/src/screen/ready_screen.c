c24239186fd59c69063f8de4fd14d03a0b50b385
StevePro7 <steveprozune@hotmail.com>
2020-12-02 15:35:02 +0000

    no message

--- dev/src/screen/ready_screen.c
+++ dev/src/screen/ready_screen.c
@@ -16,53 +16,39 @@
 
 void screen_ready_screen_load()
 {
-	//struct_hack_object *ho = &global_hack_object;
 	struct_quiz_object *qo = &global_quiz_object;
-	//unsigned char eddie_image;
-	//engine_font_manager_text( "RIFF SCREEN!!", 10, 2 );
-
 	engine_graphics_manager_clear_half();
 
 	engine_text_manager_draw_lines( 0, 2 );
-	engine_text_manager_draw_lines_extra( 2, 2, 7 );
+	engine_text_manager_draw_lines_extra( 2, 2, 9 );
+
+	engine_font_manager_text( LOCALE_READY_INFOTEXT, 1, 24 );
 	engine_text_manager_draw_lines( 4, 6 );
-	//engine_text_manager_draw_lines_extra( 8, 2, -5 );
 
-	engine_font_manager_text( LOCALE_BULLET_POINT, 2, 10 );
-	engine_font_manager_text( LOCALE_BULLET_POINT, 2, 17 );
+	engine_font_manager_text( LOCALE_BULLET_POINT, 2, 11 );
+	engine_font_manager_text( LOCALE_BULLET_POINT, 2, 20 );
 
-	engine_font_manager_text( riff_line1[ qo->quiz_riff_numbs ], 4, 10 );
-	engine_font_manager_text( riff_line2[ qo->quiz_riff_numbs ], 4, 11 );
-
-	engine_font_manager_text( diff_line1[ qo->quiz_difficulty ], 4, 17 );
-	engine_font_manager_text( diff_line2[ qo->quiz_difficulty ], 4, 18 );
-
-	//TODO delete
-	//engine_font_manager_text( "GET READY...", 4, 20 );
+	// Alternate color text.
+	engine_text_manager_draw_lines_palette( riff_line1[ qo->quiz_riff_numbs ], 4, 11 );
+	engine_text_manager_draw_lines_palette( riff_line2[ qo->quiz_riff_numbs ], 4, 12 );
+	engine_text_manager_draw_lines_palette( diff_line1[ qo->quiz_difficulty ], 4, 20 );
+	engine_text_manager_draw_lines_palette( diff_line2[ qo->quiz_difficulty ], 4, 21 );
 }
 
 void screen_ready_screen_update( unsigned char *screen_type )
 {
 	unsigned char input;
-	unsigned char input2;		// TODO delete
 
 	engine_sprite_manager_update();
-
 	input = engine_input_manager_hold_buttonA();
-	input2 = engine_input_manager_hold_right();		// TODO delete
-	if( input || input2 )
+	if( input )
 	{
 		//engine_font_manager_text( "begin", 0, 0 );
-		engine_quiz_manager_set_quiz_saved_all();
+		//engine_quiz_manager_set_quiz_saved_all();
 
-		// TODO re-instate!!
 		*screen_type = screen_type_load;
-		// TODO re-instate!!
-
-		//*screen_type = screen_type_test;
 		return;
 	}
-
 
 	engine_sprite_manager_update();
 	*screen_type = screen_type_ready;
