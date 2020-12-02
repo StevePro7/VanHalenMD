03a7a9a2c2ec792e7727160f0a9226580dac6859
StevePro7 <steveprozune@hotmail.com>
2020-12-02 15:37:07 +0000

    no message

--- dev/src/engine/hack_manager.c
+++ dev/src/engine/hack_manager.c
@@ -76,12 +76,12 @@ void engine_hack_manager_save()
 	ho->hack_delayspeed = 1;
 	//ho->hack_invincible = 1;
 
-	index = 3;
+	index = 1;
 	engine_game_manager_set_riff_select( index );
 	engine_quiz_manager_set_riffs_play( index );
 
-	//index = difficulty_type_easy;
-	index = difficulty_type_hard;
+	index = difficulty_type_easy;
+	//index = difficulty_type_hard;
 	engine_game_manager_set_diff_select( index );
 	engine_quiz_manager_set_difficulty( index );
 	// TODO delete
